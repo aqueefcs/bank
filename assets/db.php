abda80f0cd335b1da78f6731cb737d59004ecee6
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2021-03-25 16:13:29 +0530

    Update db.php

--- assets/db.php
+++ assets/db.php
@@ -1,5 +1,5 @@
 <?php 
-    $con = new mysqli('localhost','root','','mybank');
+    $con = new mysqli('remotemysql.com','JJCvmoGHpG','zDedCzDbbK','JJCvmoGHpG');
     define('bankName', 'MCB Bank',true);
     $ar = $con->query("select * from userAccounts,branch where id = '$_SESSION[userId]' AND userAccounts.branch = branch.branchId");
     $userData = $ar->fetch_assoc();
